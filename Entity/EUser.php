59b7e1d122591fd1c026096cf0b49d518557d288
Carludst <carla.distefano1@student.univaq.it>
2022-04-23 18:21:41 +0200

    user

--- Entity/EUser.php
+++ Entity/EUser.php
@@ -2,5 +2,125 @@
 
 class EUser
 {
+    private string $nome;
+    private string $cognome;
+    private string $email;
+    public string $username;
+    private string $password;
+    private bool $admin;
+
+    /**
+     * @param string $nome
+     * @param string $cognome
+     * @param string $email
+     * @param string $username
+     * @param string $password
+     * @param bool $admin
+     */
+    public function __construct(string $nome, string $cognome, string $email, string $username, string $password, bool $admin)
+    {
+        $this->nome = $nome;
+        $this->cognome = $cognome;
+        $this->email = $email;
+        $this->username = $username;
+        $this->password = $password;
+        $this->admin = $admin;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param string $nome
+     */
+    public function setNome(string $nome): void
+    {
+        $this->nome = $nome;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCognome(): string
+    {
+        return $this->cognome;
+    }
+
+    /**
+     * @param string $cognome
+     */
+    public function setCognome(string $cognome): void
+    {
+        $this->cognome = $cognome;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     */
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = hash("sha3-256", $password);
+    }
+
+    /**
+     * @return bool
+     */
+    public function getAdmin(): bool
+    {
+        return $this->admin;
+    }
+
+    /**
+     * @param bool $admin
+     */
+    public function setAdmin(bool $admin): void
+    {
+        $this->admin = $admin;
+    }
 
 }
