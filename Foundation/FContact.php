0fda0fa3e75b564fc4efa04ab59b8671291a20d1
btdav00 <101415569+btdav00@users.noreply.github.com>
2022-05-15 16:20:23 +0200

    Update FContact.php

--- Foundation/FContact.php
+++ Foundation/FContact.php
@@ -1,55 +1,77 @@
 <?php
 
-require_once "Entity/ECompetition.php";
+require_once "Entity/EContact.php";
 require_once "FDb.php";
 require_once "Entity/Etime.php";
 
 class FContact{
-    private static array $table = array("competition", "date");
+    private static array $table = array("contact");
     
-    public static function getArrayByObject(ECompetition $competition){
-        $name = $competition->getName();
-        $date = $competition->getDateTime()->format("y-m-d");
-        $time = $competition->getTime();
-        $description = $competition->getDescription();
-        $gender = $competition->getGender();
-        $competitors = $competition->getCompetitors();
-        $sport = $competition->getSport();
-        $distance = $competition->getDistance();
+    public static function getArrayByObject(EContact $contact){
+        $name = $contact->getName();
+        $number = $contact->getNumber();
+        $email = $contact->getEmail();
+        //vedere se aggiungere l'attributo evento alla classe EContact per usarlo come chiave
         $created_at = date('Y-m-d H:i:s');
         $updated_at = date('Y-m-d H:i:s');
 
         $fieldValue=array(
             'name'=>$name,
-            'date'=>$date,
-            'time'=>$time,
-            'description'=>$description,
-            'genere'=>$genere,
-            'competitors'=>$competitors,
-            'sport'=>$sport,
-            'distance'=>$distance,
-            'created_at'=>$created_at,
-            'updated_at'=>$updated_at
+            'number'=>$number,
+            'e-mail'=>$email,
+            'nameEvent'=>$nameEvent
         );
         return $fieldValue;
     }
 
-    private static function getObjectByArray(Array $competitions):ECompetition
+    private static function getObjectByArray(Array $contacts):EContacts
     {
-        $date = new DateTime($competition["date"]);
-        $object = new ECompetition($competitions["name"],$competitions[new DateTime($competitions["date"])],$competitions["time"],$competitions["description"],$competitions["gender"],$competitions["competitors"],$competitions["distance"],$created_at["created_at"],$updated_at["updated_at"]);
+        $object = new ECompetition($contacts["name"],$contacts["number"],$contacts["e-mail"],$contacts["nameEvent"]);
         return $object;
     }
 
     private static function whereKey($valueKey):String
     {
-        return FDb::where("idcompetition",$valueKey);
+        return FDb::where("nameEvent",$valueKey);
     }
 
-    public static function store(ECompetition $competition):void
+    public static function store(EContact $contact):void
     {
-        $fieldValue = self::getArrayByObject($competition,true);
+        $fieldValue = self::getArrayByObject($contact,true);
         FDb::store(self::$table[0], $fieldValue);
+    }
+
+    public static function loadOne(int $key):?EContact{
+        $query=FDb::load(self::$table[0],self::whereKey($key));
+        $result=FDb::exInterrogation($query);
+        if(count($result)==0)  return null;
+        $arrayObject=$result[0];
+        return self::getObjectByArray($arrayObject);
+    }
+
+    public static function load(int $key): Econtact{
+        $query=FDb::load(self::$table[0],$where);
+        $resultQ=FDb::exInterrogation($query,$orderBy,$ascending);
+        $result=array();
+        foreach ($resultQ as $c=>$v){
+            $result[$c]=self::getObjectByArray($v);
+        }
+        return $result;
+    }
+
+    public static function existOne(int $key):?bool
+    {
+        return FDb::exist(FDb::load(self::$table[0],self::whereKey($key)));
+    }
+
+    public static function deleteOne(int $key):?bool
+    {
+        return FDb::delate(self::$table[0],self::whereKey($key));
+    }
+
+    public static function updateOne(EContact $contact,String $idEvent):?bool
+    {
+        return FDb::update(self::$table[0],self::whereKey(array($contact->getName(),$nameEvent)),self::getArrayByObject($contact));
     }
 
 }
