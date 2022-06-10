1daf0ca74b1db13bde053fdec9ab1f0027afe23e
rober <robertodistefano24@gimail.com>
2022-06-10 18:58:25 +0200

    same change to FSession.php

--- Foundation/FSession.php
+++ Foundation/FSession.php
@@ -4,14 +4,16 @@ class FSession
 {
     public static function isLogged():bool
     {
-        session_start();
+        if (session_status() == PHP_SESSION_NONE)session_start();
         if(!is_null($_SESSION['user']))return true;
         else return false;
     }
 
     public static function login(EUser $user):void
     {
-        if (session_status() == PHP_SESSION_NONE) {
+        if (session_status() == PHP_SESSION_NONE)session_start();
+        if(!is_null($_SESSION['user']))
+        {
             $temp = serialize($user);
             $_SESSION['user'] = $temp;
             $_SESSION['user']=$user;
@@ -21,18 +23,17 @@ class FSession
 
     public static function logout()
     {
-        if(!session_status() == PHP_SESSION_DISABLED){
-            session_unset();
-            session_destroy();
-            setcookie('PHPSESSID', '', time() - 3600);
-            header('Location: /Museo/Utente/login');
-        }
-        else throw new Exception("you can't make logout if you haven't made login");
+        if (session_status() == PHP_SESSION_NONE)session_start();
+        session_unset();
+        session_destroy();
+        setcookie('PHPSESSID', '', time() - 3600);
+        header('Location: /Museo/Utente/login');
     }
 
-    public static function getLogged():?EUser
+    public static function getUserLogged():?EUser
     {
-        if(!session_status() == PHP_SESSION_DISABLED && !is_null($_SESSION['user'])){
+        if (session_status() == PHP_SESSION_NONE)session_start();
+        if(!is_null($_SESSION['user'])){
             return unserialize($_SESSION['user']);
         }
         else return NULL;
@@ -40,17 +41,17 @@ class FSession
 
     public static function addDataSession(String $key , $data)
     {
-        if(!session_status() == PHP_SESSION_DISABLED && $key!='user'){
+        if (session_status() == PHP_SESSION_NONE)session_start();
+        if($key!='user'){
             $_SESSION[$key]=serialize($data);
         }
-        else throw new Exception("you can't add date if you aren't logged");
+        else throw new Exception("invalid key for data session , it is just used for login");
     }
 
     public static function getDataSession(String $key )
     {
-        if(!session_status() == PHP_SESSION_DISABLED){
-            return $_SESSION[$key];
-        }
+        if (session_status() == PHP_SESSION_NONE)session_start();
+        if(!is_null($_SESSION[$key]))return unserialize($_SESSION[$key]);
         else return NULL;
     }
 
