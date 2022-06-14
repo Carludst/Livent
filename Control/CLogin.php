86957859b144567858a8e55908e8e139b74031af
Carludst <carla.distefano1@student.univaq.it>
2022-06-14 15:59:07 +0200

    login error

--- Control/CLogin.php
+++ Control/CLogin.php
@@ -2,21 +2,19 @@
 
 class CLogin
 {
-    static function login()
+    static function login(EUser $user)
     {
         try{
+            if(FDbH::existOne($user->getEmail(),EUser::class)){
+                $u = FDbH::loadOne($user->getEmail(), EUser::class);
 
-        }
-        catch (Exception $e){
-            //RICHIAMA ERRORE
-        }
-    }
+                if($u->getPassword() == $user->getPassword()){
+                    FSession::login($user);
+                }
+                else new Exception("password wrong");
+            }
 
-
-    static function verifica()
-    {
-        try{
-
+            else new Exception("user don't signin");
         }
         catch (Exception $e){
             //RICHIAMA ERRORE
@@ -27,7 +25,8 @@ class CLogin
     static function logout()
     {
         try{
-            FSession::logout();
+            if(FSession::isLogged()) FSession::logout();
+            else new Exception("you are not logged");
         }
         catch (Exception $e){
             //RICHIAMA ERRORE
@@ -35,43 +34,13 @@ class CLogin
     }
 
 
-    static function signin()
+    static function signin(EUser $user)
     {
         try{
+            if(FDbH::existOne($user->getEmail(), $user::class)) new Exception("user just exist");
 
-        }
-        catch (Exception $e){
-            //RICHIAMA ERRORE
-        }
-    }
-
-
-    function modificaProfilo()
-    {
-        try{
-
-        }
-        catch (Exception $e){
-            //RICHIAMA ERRORE
-        }
-    }
-
-
-    public static function update(EUser $utente)
-    {
-        try{
-
-        }
-        catch (Exception $e){
-            //RICHIAMA ERRORE
-        }
-    }
-
-
-    static function delete()
-    {
-        try{
-
+            FDbH::store($user);
+            FSession::login($user);
         }
         catch (Exception $e){
             //RICHIAMA ERRORE
