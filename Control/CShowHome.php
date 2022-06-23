ee4f43440b0d0f04e2bea3d74cfeafb8e8a4bd9e
rober <robertodistefano24@gimail.com>
2022-06-23 09:08:41 +0200

    spostato operazioni di fondation da view a controller

--- Control/CShowHome.php
+++ Control/CShowHome.php
@@ -10,7 +10,24 @@ class CShowHome
 
     public static function HomePage(){
         try{
-            //Richiama  VHome::show();
+            if(FSession::isLogged()){
+                $user=FSession::getUserLogged();
+                if(FDbH::existFile( $user,'profile'))   $profileImg=FDbH::loadFile( $user,'profile',0.2);
+                else   $profileImg=FDbH::loadFile('System/User','defaultProfile');
+            }
+            else{
+                $user=null;
+                $profileImg=null;
+            }
+
+            $homeImgName=FDbH::loadDirectory('System/HomeImg');
+            $homeImg=array();
+            for($i=0;$i<count($homeImgName);$i++)$homeImg[]=$profileImg=FDbH::loadFile( 'System/HomeImg',$homeImgName[$i]);
+            $eventFinished=FDbH::searchEvent(false,NULL,NULL,NULL,NULL,new DateTime());
+            $eventOpen=FDbH::searchEvent(true,NULL,NULL,NULL,new DateTime());
+
+
+            VHome::show($user,$profileImg,$eventOpen,$eventFinished,$homeImg);
         }
         catch(Exception $e){
             CError::store($e,"ci scusiamo per il disaggio !!! La visualizazzione della pagina home non è andata a buon fine");
@@ -21,9 +38,21 @@ class CShowHome
         try{
             if(self::authorizer()){
                 $array=FDbH::loadDirectory('System/HomeImg');
-                if(count($array)>0)$name=max($array)+1;
+                if(count($array)>0)$name=(int)max($array)+1;
                 else $name=0;
                 FDbH::storeFile('System/HomeImg',$name,$href,'type',2);
+            }
+        }
+        catch(Exception $e){
+            CError::store($e,"ci scusiamo per il disaggio !!! il file non è stato allegato , verificare di possedere le autorizazioni necessarie");
+        }
+    }
+
+    public static function setDefaultProfileImg(String $href){
+        try{
+            if(self::authorizer()){
+                //prendi input
+                FDbH::updateFile('System/User','defaultProfile',$href,'type',2);
             }
         }
         catch(Exception $e){
--- View/VHome.php
+++ View/VHome.php
@@ -1,26 +1,27 @@
 <?php
 class VHome
 {
-    private Smarty $smarty;
-    private Array $assign;
-    public function __construct(String $template, String $compile){
-        $this->smarty=new Smarty();
-        $this->smarty->setTemplateDir($template);
-        $this->smarty->setCompileDir($compile);
-        $this->assign=array();
+    private static Smarty $smarty;
+    private static String $template='home.tpl';
+
+    public function __construct(String $template, String $compile)
+    {
+        self::$smarty=new Smarty();
+        self::$smarty->setTemplateDir($template);
+        self::$smarty->setCompileDir($compile);
     }
 
-    public function show(Array $eventsProgrammed, Array $eventsFinished){
-        if(FSession::isLogged()){
-            $this->assign['user']=FSession::getUserLogged();
-            if(FDbH::existFile($this->assign['user'],'profile'))  $this->assign['profileImg']=FDbH::loadFile($this->assign['user'],'profile',0.2);
-            else  $this->assign['profileImg']=FDbH::loadFile('User','defaultProfile');
-        }
-        else{
-            $this->assign['user']=null;
-            $this->assign['profileImg']=null;
-        }
-        $this->smarty->assign($this->assign);
-        $this->smarty->display('home.tpl');
+    public static function show(?EUser $user , String $profileImg,Array $eventOpen, Array $eventsFinished , Array $homeImg)
+    {
+        $assign=array();
+
+        $assign['user']=$user;
+        $assign['profileImg']=$profileImg;
+        $assign['eventOpen']=$eventOpen;
+        $assign['eventsFinished']=$eventsFinished;
+        $assign['homeImg']=$homeImg;
+
+        self::$smarty->assign($assign);
+        self::$smarty->display(self::$template);
     }
 }
