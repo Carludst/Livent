59bb31eba81463067ad169f523692a9f8dd12ecd
rober <robertodistefano24@gimail.com>
2022-06-27 15:30:28 +0200

    add template

--- index.php
+++ index.php
@@ -2,9 +2,5 @@
 include_once "../Utility/defaultPath.php";
 include_once $GLOBALS('defoultPath')."/Utility/autoload.php";
 
-
-$blob=FDbH::loadFile('prova','r0',0.01);
-echo("<html><body><img src='data:image/jpg;base64,".$blob."'></html></body>" );
-
-//$f=new CFrontController();
-//CFrontController::run();
+$f=new CFrontController();
+CFrontController::run();
