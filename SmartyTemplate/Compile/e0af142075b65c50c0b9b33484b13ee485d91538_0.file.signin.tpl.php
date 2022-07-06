300498e7071352126f485b615e9ffabc1636d995
rober <robertodistefano24@gimail.com>
2022-07-06 17:17:12 +0200

    file inutili

---
+++ SmartyTemplate/Compile/e0af142075b65c50c0b9b33484b13ee485d91538_0.file.signin.tpl.php
@@ -0,0 +1,187 @@
+<?php
+/* Smarty version 4.1.1, created on 2022-07-06 16:43:07
+  from 'C:\xampp\htdocs\Livent\SmartyTemplate\Template\signin.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.1.1',
+  'unifunc' => 'content_62c59f7ba9a2a2_44395432',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'e0af142075b65c50c0b9b33484b13ee485d91538' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\Livent\\SmartyTemplate\\Template\\signin.tpl',
+      1 => 1657118435,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_62c59f7ba9a2a2_44395432 (Smarty_Internal_Template $_smarty_tpl) {
+?><!-- 
+THEME: Aviato | E-commerce template
+VERSION: 1.0.0
+AUTHOR: Themefisher
+
+HOMEPAGE: https://themefisher.com/products/aviato-e-commerce-template/
+DEMO: https://demo.themefisher.com/aviato/
+GITHUB: https://github.com/themefisher/Aviato-E-Commerce-Template/
+
+WEBSITE: https://themefisher.com
+TWITTER: https://twitter.com/themefisher
+FACEBOOK: https://www.facebook.com/themefisher
+-->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  ================================================== -->
+  <meta charset="utf-8">
+  <title>Livent</title>
+
+  <!-- Mobile Specific Metas
+  ================================================== -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="Construction Html5 Template">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+  <meta name="author" content="Themefisher">
+  <meta name="generator" content="Themefisher Constra HTML Template v1.0">
+  
+  <!-- Favicon -->
+  <link rel="shortcut icon" type="image/x-icon" href="<?php echo $_smarty_tpl->tpl_vars['logo']->value;?>
+" />
+  
+  <!-- Themefisher Icon font -->
+  <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['dir']->value;?>
+/plugins/themefisher-font/style.css">
+  <!-- bootstrap.min css -->
+  <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['dir']->value;?>
+/plugins/bootstrap/css/bootstrap.min.css">
+  
+  <!-- Animate css -->
+  <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['dir']->value;?>
+/plugins/animate/animate.css">
+  <!-- Slick Carousel -->
+  <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['dir']->value;?>
+/plugins/slick/slick.css">
+  <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['dir']->value;?>
+/plugins/slick/slick-theme.css">
+  
+  <!-- Main Stylesheet -->
+  <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['dir']->value;?>
+/css/style.css">
+
+</head>
+
+<body id="body">
+
+<section class="signin-page account">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <div class="block text-center">
+          <a class="logo" href="/Livent/">
+            <svg width="135px" height="29px" viewBox="0 0 155 29" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                 xmlns:xlink="http://www.w3.org/1999/xlink">
+              <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" font-size="40"
+                 font-family="AustinBold, Austin" font-weight="bold">
+                <g id="Group" transform="translate(-108.000000, -297.000000)" fill="#000000">
+                  <text id="AVIATO">
+                    <tspan x="125" y="325">Livent</tspan>
+                  </text>
+                </g>
+              </g>
+            </svg>
+          </a>
+          <h2 class="text-center">Crea il tuo Account</h2>
+          <form method="post" class="text-left clearfix" action="/Livent/User/Signin/">
+            <select class="form-control" name="type" style="height: 45px">
+              <option>Utente senza privileggi</option>
+              <option>Organizatore</option>
+            </select>
+            <br>
+            <div class="form-group">
+              <input type="text" class="form-control" name="username"  placeholder="Username">
+            </div>
+            <div class="form-group">
+              <input type="email" class="form-control" name="email" placeholder="Email">
+            </div>
+            <div class="form-group">
+              <input type="password" class="form-control"  name="password" placeholder="Password">
+            </div>
+            <div class="form-group">
+              <input type="password" class="form-control"  name="confirmPassword" placeholder="Conferma password">
+            </div>
+            <div class="text-center">
+              <button type="submit" class="btn btn-main text-center">Sign In</button>
+            </div>
+          </form>
+          <p class="mt-20">Hai già un account ?<a href="login.html"> Login</a></p>
+          <p><a href="forget-password.html"> torna alla home</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+    <!-- 
+    Essential Scripts
+    =====================================-->
+    
+    <!-- Main jQuery -->
+    <?php echo '<script'; ?>
+ src="plugins/jquery/dist/jquery.min.js"><?php echo '</script'; ?>
+>
+    <!-- Bootstrap 3.1 -->
+    <?php echo '<script'; ?>
+ src="plugins/bootstrap/js/bootstrap.min.js"><?php echo '</script'; ?>
+>
+    <!-- Bootstrap Touchpin -->
+    <?php echo '<script'; ?>
+ src="plugins/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js"><?php echo '</script'; ?>
+>
+    <!-- Instagram Feed Js -->
+    <?php echo '<script'; ?>
+ src="plugins/instafeed/instafeed.min.js"><?php echo '</script'; ?>
+>
+    <!-- Video Lightbox Plugin -->
+    <?php echo '<script'; ?>
+ src="plugins/ekko-lightbox/dist/ekko-lightbox.min.js"><?php echo '</script'; ?>
+>
+    <!-- Count Down Js -->
+    <?php echo '<script'; ?>
+ src="plugins/syo-timer/build/jquery.syotimer.min.js"><?php echo '</script'; ?>
+>
+
+    <!-- slick Carousel -->
+    <?php echo '<script'; ?>
+ src="plugins/slick/slick.min.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="plugins/slick/slick-animation.min.js"><?php echo '</script'; ?>
+>
+
+    <!-- Google Mapl -->
+    <?php echo '<script'; ?>
+ src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCC72vZw-6tGqFyRhhg5CkF2fqfILn2Tsw"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ type="text/javascript" src="plugins/google-map/gmap.js"><?php echo '</script'; ?>
+>
+
+    <!-- Main Js File -->
+    <?php echo '<script'; ?>
+ src="js/script.js"><?php echo '</script'; ?>
+>
+    
+
+
+  </body>
+  </html><?php }
+}
